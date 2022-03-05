90f888a19933630202169475cf1b2774b8764604
Miguel Basilio <lokoangel06@gmail.com>
2022-03-05 07:29:55 -0800

    CHALLENGE 11 UPDATED

--- Chapter11/main.cpp
+++ Chapter11/main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <iomanip>
 #include "input121.h"
+
+
 using namespace std;
 int menuOption();
 void Challenge6();
@@ -158,10 +160,237 @@ void Challenge8()
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+struct MonthlyBudget
+{
+    double housing = 0.0;
+    double utilities = 0.0;
+    double householdExpenses = 0.0;
+    double transportation = 0.0;
+    double food = 0.0;
+    double medical = 0.0;
+    double insurance = 0.0;
+    double entrenaiment = 0.0;
+    double clothing = 0.0;
+    double miscellaneous = 0.0;
+    double total = 0.0;
+};
+void getBudget(MonthlyBudget&);
+void totalMonthly(MonthlyBudget&);
+void bugetTotalCalculation(const double, const double);
+void displayBudget(const MonthlyBudget, const MonthlyBudget);
 void Challenge11()
 {
+    MonthlyBudget spentBudget;
+    MonthlyBudget fixedBuget = { 500.00,150.00,65.00,50.00,250.00,30.00,100.00,150.00,75.00,50.00,1420.00 };
+
+    getBudget(spentBudget);
+    totalMonthly(spentBudget);
+    displayBudget(spentBudget, fixedBuget);
 
 }
+void getBudget(MonthlyBudget &spentBudget)
+{
+    cout << "\t Please enter the total spent for each category: \n";
+    //getting all spending amounts from user:
+    cout << "\t Housing: $";
+    cin >> spentBudget.housing;
+    while (spentBudget.housing <= 0.0)
+    {
+        cout << "\t Housing: $";
+        cin >> spentBudget.housing;
+    }
+    cout << "\t Utilities: $";
+    cin >> spentBudget.utilities;
+    while (spentBudget.utilities <= 0.0)
+    {
+        cout << "\t Utilities: $";
+        cin >> spentBudget.utilities;
+    }
+    cout << "\t Household Expenses: $";
+    cin >> spentBudget.householdExpenses;
+    while (spentBudget.householdExpenses <= 0.0)
+    {
+        cout << "\t Household Expenses: $";
+        cin >> spentBudget.householdExpenses;
+    }
+    cout << "\t Transportation: $";
+    cin >> spentBudget.transportation;
+    while (spentBudget.transportation <= 0.0)
+    {
+        cout << "\t Transportation: $";
+        cin >> spentBudget.transportation;
+    }
+    cout << "\t Food: $";
+    cin >> spentBudget.food;
+    while (spentBudget.food <= 0.0)
+    {
+        cout << "\t Food: $";
+        cin >> spentBudget.food;
+    }
+    cout << "\t Medical: $";
+    cin >> spentBudget.medical;
+    while (spentBudget.medical <= 0.0)
+    {
+        cout << "\t Medical: $";
+        cin >> spentBudget.medical;
+    }
+    cout << "\t Insurance: $";
+    cin >> spentBudget.insurance;
+    while (spentBudget.insurance <= 0.0)
+    {
+        cout << "\t Insurance: $";
+        cin >> spentBudget.insurance;
+    }
+    cout << "\t Entretainment: $";
+    cin >> spentBudget.entrenaiment;
+    while (spentBudget.entrenaiment <= 0.0)
+    {
+        cout << "\t Entretainment: $";
+        cin >> spentBudget.entrenaiment;
+    }
+    cout << "\t Clothing: $";
+    cin >> spentBudget.clothing;
+    while (spentBudget.clothing <= 0.0)
+    {
+        cout << "\t Clothing: $";
+        cin >> spentBudget.clothing;
+    }
+    cout << "\t Miscellaneous: $";
+    cin >> spentBudget.miscellaneous;
+    while (spentBudget.miscellaneous <= 0.0)
+    {
+        cout << "\t Miscellaneous: $";
+        cin >> spentBudget.miscellaneous;
+    }
+}
+void totalMonthly(MonthlyBudget &spentBuget) 
+{
+    // adding all expenses to total variable:
+    spentBuget.total = spentBuget.housing + spentBuget.utilities + spentBuget.householdExpenses + spentBuget.transportation +
+        spentBuget.food + spentBuget.medical + spentBuget.insurance + spentBuget.entrenaiment + spentBuget.clothing + spentBuget.miscellaneous;
+}
+void bugetTotalCalculation(const double spentBudget, const double fixedBuget)
+{
+
+    //calculating under, over or balance budget:
+    const string overBudget = "  over budget\n";
+    const string underBudget = "  under budget!\n";
+
+    if (spentBudget > fixedBuget)
+    {
+        cout << (spentBudget - fixedBuget) << overBudget;
+    }
+    if (spentBudget < fixedBuget)
+    {
+        cout << ( fixedBuget - spentBudget) << underBudget;
+    }
+    
+}
+void displayBudget(const MonthlyBudget spentBudget, const MonthlyBudget fixedBudget)
+{
+    cout << "\n\n\tMONTHLY BUDGET - EVALUATION\n\n\n";
+
+    cout << "\tDESCRIPTION"
+        << setw(20) << right << "BUDGETED"
+        << setw(20) << right << "SPENT"
+        << setw(34) << right << "OVER / UNDER";
+
+    cout << "\n\t" << setw(97) << right << setfill('-') << "\n";
+
+    cout << setfill(' ');
+    cout << fixed << showpoint << setprecision(2);
+
+    cout << setw(20) << left << "\tHousing"
+        << setw(5) << right << "$ " << setw(7) << right << fixedBudget.housing
+        << setw(16) << right << "$ " << setw(7) << right << spentBudget.housing
+        << setw(19) << right << "$ " << setw(7) << right;
+    bugetTotalCalculation(spentBudget.housing, fixedBudget.housing);
+
+    cout << setw(20) << left << "\tUtilities"
+        << setw(5) << right << "$ " << setw(7) << right << fixedBudget.utilities
+        << setw(16) << right << "$ " << setw(7) << right << spentBudget.utilities
+        << setw(19) << right << "$ " << setw(7) << right;
+    bugetTotalCalculation(spentBudget.utilities, fixedBudget.utilities);
+
+    cout << setw(20) << left << "\tHousehold Expenses"
+        << setw(5) << right << "$ " << setw(7) << right << fixedBudget.householdExpenses
+        << setw(16) << right << "$ " << setw(7) << right << spentBudget.householdExpenses
+        << setw(19) << right << "$ " << setw(7) << right;
+    bugetTotalCalculation(spentBudget.householdExpenses, fixedBudget.householdExpenses);
+
+    cout << setw(20) << left << "\tTransport"
+        << setw(5) << right << "$ " << setw(7) << right << fixedBudget.transportation
+        << setw(16) << right << "$ " << setw(7) << right << spentBudget.transportation
+        << setw(19) << right << "$ " << setw(7) << right;
+    bugetTotalCalculation(spentBudget.transportation, fixedBudget.transportation);
+
+    cout << setw(20) << left << "\tFood"
+        << setw(5) << right << "$ " << setw(7) << right << fixedBudget.food
+        << setw(16) << right << "$ " << setw(7) << right << spentBudget.food
+        << setw(19) << right << "$ " << setw(7) << right;
+    bugetTotalCalculation(spentBudget.food, fixedBudget.food);
+
+    cout << setw(20) << left << "\tMedical"
+        << setw(5) << right << "$ " << setw(7) << right << fixedBudget.medical
+        << setw(16) << right << "$ " << setw(7) << right << spentBudget.medical
+        << setw(19) << right << "$ " << setw(7) << right;
+    bugetTotalCalculation(spentBudget.medical, fixedBudget.medical);
+
+    cout << setw(20) << left << "\tInsurance"
+        << setw(5) << right << "$ " << setw(7) << right << fixedBudget.insurance
+        << setw(16) << right << "$ " << setw(7) << right << spentBudget.insurance
+        << setw(19) << right << "$ " << setw(7) << right;
+    bugetTotalCalculation(spentBudget.insurance, fixedBudget.insurance);
+
+    cout << setw(20) << left << "\tEntertainment"
+        << setw(5) << right << "$ " << setw(7) << right << fixedBudget.entrenaiment
+        << setw(16) << right << "$ " << setw(7) << right << spentBudget.entrenaiment
+        << setw(19) << right << "$ " << setw(7) << right;
+    bugetTotalCalculation(spentBudget.entrenaiment, fixedBudget.entrenaiment);
+
+    cout << setw(20) << left << "\tClothing"
+        << setw(5) << right << "$ " << setw(7) << right << fixedBudget.clothing
+        << setw(16) << right << "$ " << setw(7) << right << spentBudget.clothing
+        << setw(19) << right << "$ " << setw(7) << right;
+    bugetTotalCalculation(spentBudget.clothing, fixedBudget.clothing);
+
+    cout << setw(20) << left << "\tMiscellaneous"
+        << setw(5) << right << "$ " << setw(7) << right << fixedBudget.miscellaneous
+        << setw(16) << right << "$ " << setw(7) << right << spentBudget.miscellaneous
+        << setw(19) << right << "$ " << setw(7) << right;
+    bugetTotalCalculation(spentBudget.miscellaneous, fixedBudget.miscellaneous);
+
+    cout << "\t" << setw(97) << right << setfill('-') << "\n";
+    cout << setfill(' ');
+
+    cout << setw(20) << left << "\tTotal"
+        << setw(5) << right << "$ " << setw(7) << right << fixedBudget.total
+        << setw(16) << right << "$ " << setw(7) << right << spentBudget.total
+        << setw(19) << right << "$ " << setw(7) << right;
+    bugetTotalCalculation(spentBudget.total, fixedBudget.total);
+
+
+
+
+
+}
+
+
+
+
+
+
 
 struct  StudentGrade
 {
